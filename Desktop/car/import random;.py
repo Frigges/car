cdeb8e78c3d3381006f97b720b24fbbf00cc65d2
Frigges <jonatap@chalmers.se>
2024-11-06 09:14:20 +0100

    stuff

---
+++ Desktop/car/import random;.py
@@ -0,0 +1,33 @@
+import random;
+import
+
+def main():
+
+
+
+    class driveline():
+        mass=0
+        vIn=0
+        amp=0
+        
+        def motor():
+            vIn
+            amp
+        def gearbox(t, w):
+            w1=w/3                      # Vridmoment  w
+            w2=w1/3                     # 
+            r1=(w*0.005)/w1             #
+            vrid=(r1*w1)/w2               #
+            torque=(t*9*0.95**2)
+            return driveline.wheels(torque, vrid) 
+        def wheels(t,v):
+            print("hello")
+
+    class sim():
+        angle=0
+        traction=0
+        gravity= 9.82
+
+        def time():
+            print("hello")
+        def 
